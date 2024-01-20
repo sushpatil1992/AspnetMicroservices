cfa193cc85811ecd0de53a0cd8d7b618a05bb058
sushpatil1992 <sushpatil1992@gmail.com>
2024-01-20 22:59:14 +0530

    Added docker and basket API

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -7,7 +7,7 @@ namespace Catalog.API.Data
     {
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionSring"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
